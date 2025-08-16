9434d9fbc5d834ef1652a86b7883675ef1c07ee1
codechiefVignesh <vignesharavindh72@gmail.com>
2025-08-16 12:10:32 +0530

    Fix Vercel deployment

--- build_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Build script for Vercel
-echo "Building static files..."
-py -3.10 -m pip install -r requirements.txt
-py -3.10 manage.py collectstatic --noinput --clear
--- online_judge/wsgi.py
+++ online_judge/wsgi.py
@@ -1,16 +1,20 @@
 """
 WSGI config for online_judge project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+Updated for Vercel deployment.
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Add the project directory to Python path
+project_home = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if project_home not in sys.path:
+    sys.path.append(project_home)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_judge.settings')
 
 application = get_wsgi_application()
+
+# Vercel requires this
+app = application
