55fd258dc35d85ffa140e04043aeb020e737d07e
codechiefVignesh <vignesharavindh72@gmail.com>
2025-08-16 11:34:25 +0530

    Add Vercel configuration

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,5 @@
 
 # Build script for Vercel
 echo "Building static files..."
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput --clear
+py -3.10 -m pip install -r requirements.txt
+py -3.10 manage.py collectstatic --noinput --clear
